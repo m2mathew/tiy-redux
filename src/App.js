22dbaa3c7f850accaeb31b1722b7c372b4ab429b
Mike Mathew <m2mathew@me.com>
2017-05-16 00:31:43 -0500

    Adds MUI and style tweaking

--- src/App.js
+++ src/App.js
@@ -1,19 +1,62 @@
+// External Dependencies
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
 import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+import { List, ListItem } from 'material-ui/List';
+
+// Internal Dependencies
 import logo from './logo.svg';
 import './App.css';
 
+// Needed for onTouchTap event handling
+injectTapEventPlugin();
+
+// Create the redux store
+// const store = initializeStore();
+
+
+// Component Definition
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <MuiThemeProvider>
+        <div className="App">
+
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <h2>React + Redux</h2>
+          </div>
+
+          <Paper className="App-main">
+            Let's start by making something to put here...
+
+            <div className="data-display">
+              {/* TODO: map over data to display here */}
+              <ListItem primaryText="Caitlin" />
+              <ListItem primaryText="Michael" />
+              <ListItem primaryText="Eric" />
+              <ListItem primaryText="Colin" />
+
+            </div>
+
+            <div className="main-form">
+              <TextField
+                floatingLabelText="Enter first name"
+              />
+              <RaisedButton
+                className="submit-button"
+                label="Submit"
+                primary
+              />
+            </div>
+
+          </Paper>
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    </MuiThemeProvider>
     );
   }
 }
